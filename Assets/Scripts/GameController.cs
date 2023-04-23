b4234f2644711c61a3422723fc695df20e46637a
ItzelMarcial <itzel.marcialno@alumno.buap.mx>
2023-04-23 17:04:48 -0600

    Prueba de botones
    Hice una prueba rápida de botones uwu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController Instancia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instancia = this;
+    }
+
+    public void CambiarAEscena(int numeroEscena)
+    {
+        SceneManager.LoadScene(numeroEscena);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
