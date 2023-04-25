8d8c13941183ebf2f2e6e6cce6c18259d3b9a9e7
alxsctt <karla.castillome@alumno.buap.mx>
2023-04-24 23:14:04 -0600

    Lectura de texto más dinámica y cambio de escenas según las opciones :3
    uwu nya

--- Assets/Scripts/ReadingText.cs
+++ Assets/Scripts/ReadingText.cs
@@ -11,19 +11,25 @@ public class ReadingText : MonoBehaviour
     //para el diálogo
     public TextAsset textFile;     // drop your file here in inspector
     string[] linesArray;
-    string myFilePath, filename;
+    
     int linepos = 0;
 
     //elementos en la UI
     [SerializeField] TextMeshProUGUI txt_dialogo;
+
+    public Button opcion1;
+    public Button opcion2;
     
     // Start is called before the first frame update
     void Start()
     {
-        //string text = textFile.text;  //this is the content as string
-        filename = "Esc01.txt";
-        myFilePath = Application.dataPath + "/" + filename;
+        //borramos cualquier texto que pueda haber en pantalla
         txt_dialogo.text = "";
+
+        //desactivamos los botones
+        opcion1.gameObject.SetActive(false); 
+        opcion2.gameObject.SetActive(false);
+
         ReadFromTheFile();
     }
 
@@ -35,20 +41,30 @@ public class ReadingText : MonoBehaviour
 
     void ReadFromTheFile()
     {
-        linesArray = File.ReadAllLines(myFilePath);
+        //guardamos las oraciones del archivo de texto en un array
+        linesArray = textFile.text.Split("\n");
     }
 
     //este método se llama desde el botón cuando es presionado
     public void DisplayText()
     {
+        Debug.Log(linepos);
+        Debug.Log(linesArray.Length);
+
         if(linepos >= linesArray.Length)
         {
             //si ya acabó el diálogo, hay que llamar al menú de opciones
+            opcion1.gameObject.SetActive(true);
+            opcion2.gameObject.SetActive(true);
+
             return;
         }
-        txt_dialogo.text = "";
+        else
+        {
+            txt_dialogo.text = "";
 
-        txt_dialogo.text += linesArray[linepos] + "\n";
-        linepos++;
+            txt_dialogo.text += linesArray[linepos] + "\n";
+            linepos++;
+        }
     }
 }
